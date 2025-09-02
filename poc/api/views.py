ce06a1f51f4875989d4b3bee4e73622aab464cbb
Magesh Ravi <magesh@webinative.com>
2025-09-02 17:54:42 +0530

    feat: [#30] List Create Thread API
    - creates a new API endpoint for listing and creating chat threads.
    - renames existing view function for consistency.

--- poc/api/views.py
+++ poc/api/views.py
@@ -1,9 +1,17 @@
 from rest_framework.generics import ListCreateAPIView
+from rest_framework.permissions import IsAuthenticated
 
-from poc.api.serializers import UploadedFileSerializer
-from poc.models import UploadedFile
+from poc.api.serializers import ChatThreadSerializer, UploadedFileSerializer
+from poc.models import ChatThread, UploadedFile
 
 
-class UploadedFileCreateAPI(ListCreateAPIView):
+class ListCreateUploadedFileAPI(ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     serializer_class = UploadedFileSerializer
     queryset = UploadedFile.objects.all().order_by("-id")
+
+
+class ListCreateThreadAPI(ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = ChatThreadSerializer
+    queryset = ChatThread.objects.all().order_by("-id")

2acc9a5a6ed81acdd7a1aecdcbcd1619cc15d90c
Magesh Ravi <magesh@webinative.com>
2025-11-27 22:55:14 +0530

    test: [#47] search litigant
    - adds unit tests for search functionality in list litigants page.

--- poc/tests/api/views/test_list_litigants_api.py
+++ poc/tests/api/views/test_list_litigants_api.py
@@ -26,3 +26,23 @@ def test_happy_path(api_client, litigants, users):
     returned_names = {litigant["name"] for litigant in response.data["results"]}
     expected_names = {litigants["mahadevan"].name, litigants["gopalan"].name}
     assert returned_names == expected_names
+
+
+def test_search(api_client, litigants, users):
+    api_client.force_authenticate(user=users["user1"])
+    response = api_client.get(_get_api_url(), {"search": "van"})
+    assert response.status_code == 200
+    assert response.data["count"] == 1
+    assert response.data["results"][0]["name"] == litigants["mahadevan"].name
+
+    response = api_client.get(_get_api_url(), {"search": "director"})
+    assert response.status_code == 200
+    assert response.data["count"] == 2
+
+    response = api_client.get(_get_api_url(), {"search": "601"})
+    assert response.status_code == 200
+    assert response.data["count"] == 1
+
+    response = api_client.get(_get_api_url(), {"search": "com"})
+    assert response.status_code == 200
+    assert response.data["count"] == 2

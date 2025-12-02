b2c64fc67aad3f54e4ca7372e8618cd1634ebc32
Magesh Ravi <magesh@webinative.com>
2025-12-02 06:44:03 +0530

    refactor: [#49] init alpha script
    - formats the output of the init alpha script to make it readable.
    - adds the missing ENV vars for creating superuser without user input.

--- _init_alpha.sh
+++ _init_alpha.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+CYAN_BG=$(tput setab 6)
+BOLD=$(tput bold)
+RESET=$(tput sgr0)
+
+printf "${CYAN_BG}${BOLD} Applying database migrations... ${RESET}\n"
 python manage.py migrate
+
+printf "${CYAN_BG}${BOLD} Creating superuser... ${RESET}\n"
 python manage.py createsuperuser --noinput
